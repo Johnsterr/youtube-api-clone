1478012ffec0b57b40e6a78b9e4763028e93dd0e
Evgeniy Pashko <evgpashko92@gmail.com>
2022-06-22 23:46:54 +0700

    Конфигурация провайдера для модульной структуры

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://crm.loc'),
 
     'asset_url' => env('ASSET_URL'),
 
@@ -194,6 +194,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\ModularProvider::class
 
     ],
 
