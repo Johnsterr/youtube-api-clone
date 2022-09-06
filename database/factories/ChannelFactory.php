2b46a7b29c8c7a6739b3e7395e8e5f10ed5414bb
Evgeniy Pashko <evgpashko92@gmail.com>
2022-09-06 23:28:03 +0700

    Фабрики (Factories) - генерация тестовых данных

---
+++ database/factories/ChannelFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Channel>
+ */
+class ChannelFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
+            'user_id' => User::inRandomOrder()->first()->id
+        ];
+    }
+}
