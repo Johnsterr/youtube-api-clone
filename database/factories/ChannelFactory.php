83826999671e67f30b2a0988fcf096d636d97646
Evgeniy Pashko <evgpashko92@gmail.com>
2022-09-07 11:22:29 +0700

    Убрал явный вызов id в фабрике (Laravel автоматически определит id)

--- database/factories/ChannelFactory.php
+++ database/factories/ChannelFactory.php
@@ -19,7 +19,7 @@ class ChannelFactory extends Factory
     {
         return [
             'name' => ucfirst($this->faker->words(mt_rand(1, 2), true)),
-            'user_id' => User::inRandomOrder()->first()->id
+            'user_id' => User::inRandomOrder()->first()
         ];
     }
 }
