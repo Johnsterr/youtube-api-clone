d6004be6ca8295ef391bbc71277a5ab6c260f9a4
Evgeniy Pashko <evgpashko92@gmail.com>
2023-06-01 23:53:50 +0700

    Обновление комментариев

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::get('/videos/{video}', [VideoController::class, 'show']);
 Route::get('/comments', [CommentController::class, 'index']);
 Route::get('/comments/{comment}', [CommentController::class, 'show']);
 Route::post('/comments', [CommentController::class, 'store']);
+Route::put('/comments/{comment}', [CommentController::class, 'update']);
 
 Route::get('/users', [UserController::class, 'index']);
 Route::get('/users/{user}', [UserController::class, 'show']);
